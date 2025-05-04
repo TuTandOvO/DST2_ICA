a547b89a9fc20e3b2b1cb9223d423c6927f941ce
TuTandOvO <yixiangren99@gmail.com>
2025-05-04 23:25:05 +0800

    修改初始界面和index的不匹配问题

--- src/main/java/cn/edu/zju/controller/IndexController.java
+++ src/main/java/cn/edu/zju/controller/IndexController.java
@@ -1,5 +1,8 @@
 package cn.edu.zju.controller;
 
+import cn.edu.zju.dao.DiseaseDao;
+import cn.edu.zju.dao.DosingDao;
+import cn.edu.zju.dao.GeneDao;
 import cn.edu.zju.servlet.DispatchServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +21,17 @@ public class IndexController {
     }
 
     public void index(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        GeneDao geneDao = new GeneDao();
+        DosingDao dosingDao = new DosingDao();
+        DiseaseDao diseaseDao = new DiseaseDao();
+
+        int geneCount = geneDao.getGeneCount();  // 从 gene_info 表获取基因数
+        int dosingCount = dosingDao.getDosageCount();  // 从 dosing_guideline 表获取行数
+        int diseaseCount = diseaseDao.getDiseaseCount();  // 从 disease_info 表获取行数
+
+        request.setAttribute("geneCount", geneCount);
+        request.setAttribute("dosingCount", dosingCount);
+        request.setAttribute("diseaseCount", diseaseCount);
         request.getRequestDispatcher("/views/index.jsp").forward(request, response);
 
     }
